d8a9cdab12b60455aa08f0fd2357fb5db4b48246
UncleCamel <davionxiang@gmail.com>
2014-01-02 13:45:55 +0800

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -20,7 +20,7 @@ describe "Authentication" do
     end
 
 
-  describe "with valid information" do
+    describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before { sign_in user}
 
@@ -51,8 +51,7 @@ describe "Authentication" do
           click_button "Sign in"
         end
 
-		describe "after signing in" do
-
+		    describe "after signing in" do
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
           end
@@ -74,6 +73,19 @@ describe "Authentication" do
         describe "visiting the user index" do
           before { visit users_path }
           it { should have_title('Sign in') }
+        end
+      end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
         end
       end
     end
